174271f61e545a178d55ce94f8f83812dc7cd6e4
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-12-15 00:34:40 +0100

    debug moved in core

--- test/main.cpp
+++ test/main.cpp
@@ -1,30 +1,21 @@
 //
-//  main.cpp
+//  main.m
 // created automatically
 //
 //
-//  
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
+#ifndef DEBUG
+#define DEBUG
+#endif
+#include <common/debug/core/debug.h>
 
-#include "__template_name__.h"
+using namespace common::debug;
 
 int main(int argc, char *argv[])
 {
-  boost::program_options::options_description desc("options");
-  desc.add_options()("help", "help");
-  // put your additional options here
 
-  //////
-  boost::program_options::variables_map vm;
-  boost::program_options::store(boost::program_options::parse_command_line(argc,argv, desc),vm);
-  boost::program_options::notify(vm);
-  
-  if (vm.count("help")) {
-    std::cout << desc << "\n";
-    return 1;
-  }  
-  // test your lib__template_name__ library here
-
+  DPRINT("Hello World\n");
   return 0;
 }
